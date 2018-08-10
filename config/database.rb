374672239fcf3b4b5dd6db8784edfa5047b478ac
sabafarheen <sabafarheen2003@gmail.com>
2018-08-10 01:49:49 +0000

    Hi

--- config/database.rb
+++ config/database.rb
@@ -4,11 +4,7 @@ configure do
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-  set :database, {
-    adapter: "sqlite3",
-    database: "db/db.sqlite3"
-  }
-
+  
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
   Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
@@ -16,7 +12,7 @@ configure do
     autoload ActiveSupport::Inflector.camelize(filename), model_file
   end
 
-end
+
 
 if Sinatra::Application.development?
     set :database, {
@@ -34,3 +30,4 @@ if Sinatra::Application.development?
       encoding: 'utf8'
     }
   end
+end
